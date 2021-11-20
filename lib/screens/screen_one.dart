2f70ed08b77319b7a0bd0d613ca11a64b6851e2c
Alihan Gedik <gedikhan44@gmail.com>
2021-11-20 20:55:23 +0300

    Update screen_one.dart

--- lib/screens/screen_one.dart
+++ lib/screens/screen_one.dart
@@ -1,4 +1,4 @@
-import 'package:ad_gu/model/screens.dart';
+import 'package:ad_gu/widgets/screens.dart';
 import 'package:flutter/material.dart';
 
 class ScreenOne extends StatefulWidget {
@@ -17,27 +17,13 @@ class _ScreenOneState extends State<ScreenOne> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children:  [
-            const Screens(
+          children:  const [
+            Screens(
               image: "assets/images/bg layouts.png",
               image2: "assets/images/content1-1.png",
               topValue: -135,
               leftValue: -91,
             ),
-            /* Container(
-              width: 300,
-              child: const Text(
-                "Popularised in the 1960s with the release of Letraset sheets co", textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 24, fontFamily:"Poppins-ExtraBold"),
-              ),
-            ),
-            Container(
-              width: 300,
-              child: const Text(
-                "Popularised in the 1960s with the release of Letraset sheets co", textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 24, fontFamily:"Poppins-ExtraBold"),
-              ),
-            ), */
           ],
         ),
       ),
