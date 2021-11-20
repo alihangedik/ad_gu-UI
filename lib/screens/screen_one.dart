a52f557742e60b5294d52ea19c76c67cd2e2b3cf
Alihan Gedik <gedikhan44@gmail.com>
2021-11-20 17:46:47 +0300

    Update screen_one.dart

--- lib/screens/screen_one.dart
+++ lib/screens/screen_one.dart
@@ -11,13 +11,21 @@ class ScreenOne extends StatefulWidget {
 class _ScreenOneState extends State<ScreenOne> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
-      body: Screens(
-        image: "assets/images/bg layouts.png",
-        image2: "assets/images/content1.png",
-        topValue: -135,
-        leftValue: -91,
+      body: Column(
+        children: const [
+          Screens(
+            image: "assets/images/bg layouts.png",
+            image2: "assets/images/content1.png",
+            topValue: -135,
+            leftValue: -91,
+          ),
+          Text(
+            "Popularised in the 1960s with the release of Letraset sheets co",
+            style: TextStyle(fontSize: 2),
+          )
+        ],
       ),
     );
   }
